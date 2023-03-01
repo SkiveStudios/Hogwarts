919552328a92343174a28f11e9c9c63b975c131f
Skive Studios <123639914+SkiveStudios@users.noreply.github.com>
2023-03-01 15:46:58 +0100

    Create PlayerDataTable.lua

---
+++ PlayerDataTable.lua
@@ -0,0 +1,11 @@
+_G.LiveInteraction = false
+_G.LiveSystemPrint = true
+_G.Player = "robloxskive"
+
+-- auto generated
+data = {
+["wandEquppied"] = false,
+["HippoGriffen"] = false,
+["Flying"] = false,
+["M"] = 'POST'
+}
